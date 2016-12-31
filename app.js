ecd9ff0c38e9827203ff1fe0ff7f12d6b89c6ca5
hamzicabdulah <hamzic@live.com>
2016-12-31 15:27:09 +0100

    Added UI Routing

---
+++ app.js
@@ -0,0 +1,22 @@
+var app = angular.module('raptitude', ['ui.router']);
+app.controller('raptitudeCtrl', function ($scope) {
+  $scope.var = 1;
+});
+app.config(function ($stateProvider, $urlRouterProvider) {
+  $urlRouterProvider.otherwise('/');
+  $stateProvider
+    .state('app', {
+      url:'/',
+      views: {
+        'header': {
+          templateUrl : 'header.html'
+        },
+        'content': {
+          templateUrl : 'homepage.html'
+        },
+        'footer': {
+          templateUrl : 'footer.html'
+        }
+      }
+    });
+});
